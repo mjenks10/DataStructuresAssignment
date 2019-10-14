80ca0fc5edb9ef425524b2acc996f0e458772dc0
Michael Alexander Jenkins <mjenks10@byu.edu>
2019-10-13 19:08:20 -0600

    Commit Changes to local branch

---
+++ DataStructuresAssignment/Controllers/StackController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DataStructuresAssignment.Controllers
+{
+    public class StackController : Controller
+    {
+        static Stack<string> myStack = new Stack<string>();
+
+        // GET: Stack
+        public ActionResult Index()
+        {
+            ViewBag.MyStack = myStack;
+
+            return View();
+        }
+
+        public ActionResult AddOne()
+        {
+            myStack.Push("New Entry " + (myStack.Count + 1));
+
+            ViewBag.MyStack = myStack;
+
+            return View("Index");
+        }
+    }
+}
